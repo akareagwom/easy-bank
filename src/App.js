9808063a9abe8373ab61852c6518bdb5b4880dee
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-04-11 21:21:21 +0100

    background

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Button from './components/button';
 import Navbar from './components/navbar';
 import mockups from './images/image-mockups.png'
 // import intro from './images/bg-intro.png'
@@ -11,14 +12,17 @@ function App() {
         <Navbar/>
       </div>
 
-      <div className='bg-intromobile bg-no-repeat h-screen'>
-        <div className='absolute p-4 top-[-13%] z-20'>
-          <img src={mockups} alt="asset"/>
+      <div className=' h-screen'>
+        <div className='absolute bg-intromobile bg-no-repeat p-4 top-[0%] z-[-1] '>
+          <img  src={mockups} alt="asset"/>
         </div>
-        <div className='pt-[80%]'>
-          <h1>Next generation digital banking</h1>
+        <div className='pt-[95%] px-[9.5%] text-center'>
+          <h1 className='text-3xl text-[hsl(233, 8%, 62%)]'>Next generation digital banking</h1>
           <p>Take your financial life online. Your Easybank account will be a one-stop-shop 
            for spending, saving, budgeting, investing, and much more.</p>
+        </div>
+        <div>
+          <Button/>
         </div>
       </div>
 
--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,7 +1,7 @@
 const Button = () => {
     return ( 
         <>
-            
+            <button className="bg-gradient-to-r from-green-500 via-cyan-400 to-blue-500">Request Invite</button>
         </>
      );
 }
