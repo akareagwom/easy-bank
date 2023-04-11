a29676c6d840ce6525f537253a3b78af86317e8b
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-04-11 23:16:39 +0100

    card1 section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Button from './components/button';
 import Navbar from './components/navbar';
 import mockups from './images/image-mockups.png'
 import onboarding from './images/icon-onboarding.svg'
+import online from './images/icon-online.svg'
+import budgeting from './images/icon-budgeting.svg'
+import api from './images/icon-api.svg'
 
 
 function App() {
@@ -24,27 +27,36 @@ function App() {
         <div className=''>
           <Button/>
         </div>
-        <div className='text-center px-[9.5%] bg-slate-100'>
-        <div>
+        <div className=' px-[9.5%] bg-slate-100'>
+        <div className='text-center'>
           <h1 className='text-3xl'>Why choose Easybank?</h1>
           <p>We leverage Open Banking to turn your bank account into your financial hub. Control 
           your finances like never before.</p>
         </div>
-        <div>
+        <div className='block justify-center m-0 items-center'>
           <div>
-            <img src ={onboarding} alt=""/>
-            <h2></h2>
-            <p></p>
+            <img src ={online} alt=""/>
+            <h2>Online Banking</h2>
+            <p>Our modern web and mobile applications allow you to keep track of your finances 
+            wherever you are in the world.</p>
+          </div>
+          <div>
+            <img src ={budgeting} alt=""/>
+            <h2>Simple budgeting</h2>
+            <p>See exactly where your money goes each month. Receive notifications when you’re 
+            close to hitting your limits.</p>
           </div>
           <div>
             <img src ={onboarding} alt=""/>
-            <h2></h2>
-            <p></p>
+            <h2>Fast Onboarding</h2>
+            <p>We don’t do branches. Open your account in minutes online and start taking control 
+            of your finances right away.</p>
           </div>
           <div>
-            <img src ={onboarding} alt=""/>
-            <h2></h2>
-            <p></p>
+            <img src ={api} alt=""/>
+            <h2>Open API</h2>
+            <p>Manage your savings, investments, pension, and much more from one account. Tracking 
+            your money has never been easier.</p>
           </div>
         </div>
       </div>
