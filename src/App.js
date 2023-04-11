9655eaf0b81e858beaa0ee0f99bd08bf932a490b
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-04-11 23:29:37 +0100

    centred images

--- src/App.js
+++ src/App.js
@@ -33,28 +33,28 @@ function App() {
           <p>We leverage Open Banking to turn your bank account into your financial hub. Control 
           your finances like never before.</p>
         </div>
-        <div className='block justify-center m-0 items-center'>
+        <div className='block text-center w-full justify-center  items-center'>
           <div>
-            <img src ={online} alt=""/>
-            <h2>Online Banking</h2>
+            <img className="ml-[35%] my-[5%]"  src ={online} alt=""/>
+            <h2 className="text-2xl my-[5%]">Online Banking</h2>
             <p>Our modern web and mobile applications allow you to keep track of your finances 
             wherever you are in the world.</p>
           </div>
           <div>
-            <img src ={budgeting} alt=""/>
-            <h2>Simple budgeting</h2>
+            <img className="ml-[35%] my-[5%]" src ={budgeting} alt=""/>
+            <h2 className="text-2xl my-[5%]">Simple budgeting</h2>
             <p>See exactly where your money goes each month. Receive notifications when you’re 
             close to hitting your limits.</p>
           </div>
           <div>
-            <img src ={onboarding} alt=""/>
-            <h2>Fast Onboarding</h2>
+            <img className="ml-[35%] my-[5%]" src ={onboarding} alt=""/>
+            <h2 className="text-2xl my-[5%]">Fast Onboarding</h2>
             <p>We don’t do branches. Open your account in minutes online and start taking control 
             of your finances right away.</p>
           </div>
           <div>
-            <img src ={api} alt=""/>
-            <h2>Open API</h2>
+            <img className="ml-[35%] my-[5%]" src ={api} alt=""/>
+            <h2 className="text-2xl my-[5%]">Open API</h2>
             <p>Manage your savings, investments, pension, and much more from one account. Tracking 
             your money has never been easier.</p>
           </div>
