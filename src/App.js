da09047d1555fd92681ff7fc01fdcf04c25c223d
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-04-11 23:55:11 +0100

    article card2

--- src/App.js
+++ src/App.js
@@ -69,27 +69,31 @@ function App() {
           <h1 className='text-center text-3xl' >Latest Articles</h1>
           <div>
             <img src={currency} alt=''/>
-            <h4></h4>
-            <h2></h2>
-            <p></p>
+            <h4>By Claire Robinson</h4>
+            <h2>Receive money in any currency with no fees</h2>
+            <p>The world is getting smaller and we’re becoming more mobile. So why should you be 
+            forced to only receive money in a single …</p>
           </div>
           <div>
             <img src={restaurant} alt=''/>
-            <h4></h4>
-            <h2></h2>
-            <p></p>
+            <h4>By Wilson Hutton</h4>
+            <h2>Treat yourself without worrying about money</h2>
+            <p>Our simple budgeting feature allows you to separate out your spending and set 
+            realistic limits each month. That means you …</p>
           </div>
           <div>
             <img src={plane} alt=''/>
-            <h4></h4>
-            <h2></h2>
-            <p></p>
+            <h4>By Wilson Hutton</h4>
+            <h2>Take your Easybank card wherever you go</h2>
+            <p>We want you to enjoy your travels. This is why we don’t charge any fees on purchases 
+            while you’re abroad. We’ll even show you …</p>
           </div>
           <div>
             <img src={confetti} alt=''/>
-            <h4></h4>
-            <h2></h2>
-            <p></p>
+            <h4>By Claire Robinson</h4>
+            <h2>Our invite-only Beta accounts are now live!</h2>
+            <p>After a lot of hard work by the whole team, we’re excited to launch our closed beta. 
+            It’s easy to request an invite through the site ...</p>
           </div>
       </div>
       </div>
