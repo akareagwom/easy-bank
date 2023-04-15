e62731e2ef0c44a13167f09f32290d7b1abf6fba
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-04-15 20:05:45 +0100

    3rd section

--- src/App.js
+++ src/App.js
@@ -28,9 +28,9 @@ function App({images}) {
       </div>
 
       <div>
-        <div className=' md:flex '>
+        <div className=' md:flex w-full '>
           <div className='absolute bg-intromobile  h-auto md:h-screen md:bg-[0%] md:bg-introdesktop md:left-[35%] md:z-[-2] md:top-[-5%] bg-no-repeat p-4 top-[0%] z-[-1] '>
-            <img className='w-[70%] md:ml-[30%]' src={mockups} alt="asset"/>
+            <img className='w-[70%] md:w-[90%] md:ml-[30%]' src={mockups} alt="asset"/>
           </div>
           <div className='pt-[99%] md:pt-[10%] md:pr-[57%]  px-[9.5%] text-center'>
             <h1 className='text-3xl md:text-3xl text-slate-700 text-[hsl(233, 8%, 62%)]'>Next generation digital banking</h1>
@@ -43,34 +43,34 @@ function App({images}) {
           
         </div>
         <div className=' px-[9.5%] py-[10%] bg-slate-100'>
-        <div className='text-center md:text-start'>
-          <h1 className='text-3xl'>Why choose Easybank?</h1>
-          <p>We leverage Open Banking to turn your bank account into your financial hub. Control 
+        <div className='text-center md:text-start md:w-[80%]'>
+          <h1 className='text-3xl md:text-[20px]'>Why choose Easybank?</h1>
+          <p lassName=' md:text-[12px]'>We leverage Open Banking to turn your bank account into your financial hub. Control 
           your finances like never before.</p>
         </div>
-        <div className='block text-center w-full justify-center  items-center'>
+        <div className='block text-center md:text-start w-full justify-center md:flex  items-center'>
           <div>
             <img className="ml-[35%] my-[5%]"  src ={online} alt=""/>
-            <h2 className="text-2xl my-[5%]">Online Banking</h2>
-            <p>Our modern web and mobile applications allow you to keep track of your finances 
+            <h2 className="text-2xl md:text-[20px] my-[5%]">Online Banking</h2>
+            <p className='md:text-[12px]'>Our modern web and mobile applications allow you to keep track of your finances 
             wherever you are in the world.</p>
           </div>
           <div>
             <img className="ml-[35%] my-[5%]" src ={budgeting} alt=""/>
-            <h2 className="text-2xl my-[5%]">Simple budgeting</h2>
-            <p>See exactly where your money goes each month. Receive notifications when you’re 
+            <h2 className="text-2xl md:text-[20px] my-[5%]">Simple budgeting</h2>
+            <p className='md:text-[12px]'>See exactly where your money goes each month. Receive notifications when you’re 
             close to hitting your limits.</p>
           </div>
           <div>
-            <img className="ml-[35%] my-[5%]" src ={onboarding} alt=""/>
-            <h2 className="text-2xl my-[5%]">Fast Onboarding</h2>
-            <p>We don’t do branches. Open your account in minutes online and start taking control 
+            <img className="ml-[35%]  my-[5%]" src ={onboarding} alt=""/>
+            <h2 className="text-2xl md:text-[20px] my-[5%]">Fast Onboarding</h2>
+            <p className='md:text-[12px]'>We don’t do branches. Open your account in minutes online and start taking control 
             of your finances right away.</p>
           </div>
           <div>
             <img className="ml-[35%] my-[5%]" src ={api} alt=""/>
-            <h2 className="text-2xl my-[5%]">Open API</h2>
-            <p>Manage your savings, investments, pension, and much more from one account. Tracking 
+            <h2 className="text-2xl md:text-[20px] my-[5%]">Open API</h2>
+            <p className='md:text-[12px]'>Manage your savings, investments, pension, and much more from one account. Tracking 
             your money has never been easier.</p>
           </div>
         </div>
@@ -78,40 +78,42 @@ function App({images}) {
       </div>
       <div className='px-[5%] py-[5%] bg-slate-50'>
           <h1 className='text-center text-3xl' >Latest Articles</h1>
-          <div className="my-[7%] bg-white rounded-[10px] shadow-xl">
-            <img className="rounded-t-[10px]" src={currency} alt=''/>
-            <div className='p-[7%] '>
-              <h4 className="text-[12px]">By Claire Robinson</h4>
-              <h2 className="text-[25px] " >Receive money in any currency with no fees</h2>
-              <p className="text-[16px] " >The world is getting smaller and we’re becoming more mobile. So why should you be 
-              forced to only receive money in a single …</p>
+          <div className='md:flex md:justify-around'>
+            <div className="my-[7%] md:w-[80%] md:mx-[1%] bg-white rounded-[10px] shadow-xl">
+              <img className="rounded-t-[10px]" src={currency} alt=''/>
+              <div className='p-[7%]  '>
+                <h4 className="md:text-[10px] text-[12px]">By Claire Robinson</h4>
+                <h2 className="text-[25px] md:text-[16px] " >Receive money in any currency with no fees</h2>
+                <p className="text-[16px] md:text-[12px] " >The world is getting smaller and we’re becoming more mobile. So why should you be 
+                forced to only receive money in a single …</p>
+              </div>
             </div>
-          </div>
-          <div className="my-[7%] bg-whi rounded-[10px] shadow-xl">
-          <img className="rounded-t-[10px]" src={restaurant} alt=''/>
-            <div className='p-[7%]' >
-              <h4 className="text-[12px]">By Wilson Hutton</h4>
-              <h2 className="text-[25px] " >Treat yourself without worrying about money</h2>
-              <p className="text-[16px] " >Our simple budgeting feature allows you to separate out your spending and set 
-              realistic limits each month. That means you …</p>
+            <div className="my-[7%] md:w-[80%] md:mx-[1%] bg-white rounded-[10px] shadow-xl">
+            <img className="rounded-t-[10px]" src={restaurant} alt=''/>
+              <div className='p-[7%]' >
+                <h4 className="md:text-[10px] text-[12px]">By Wilson Hutton</h4>
+                <h2 className="text-[25px] md:text-[16px] " >Treat yourself without worrying about money</h2>
+                <p className="text-[16px] md:text-[12px] " >Our simple budgeting feature allows you to separate out your spending and set 
+                realistic limits each month. That means you …</p>
+              </div>
             </div>
-          </div>
-          <div className="my-[7%] bg-white rounded-[10px] shadow-xl">
-            <img className="rounded-t-[10px]" src={plane} alt=''/>
-            <div className='p-[7%]' >
-              <h4 className="text-[12px]">By Wilson Hutton</h4>
-              <h2 className="text-[25px] " >Take your Easybank card wherever you go</h2>
-              <p className="text-[16px] " >We want you to enjoy your travels. This is why we don’t charge any fees on purchases 
-              while you’re abroad. We’ll even show you …</p>
+            <div className="my-[7%] md:w-[80%] md:mx-[1%] bg-white rounded-[10px] shadow-xl">
+              <img className="rounded-t-[10px]" src={plane} alt=''/>
+              <div className='p-[7%]' >
+                <h4 className="md:text-[10px] text-[12px]">By Wilson Hutton</h4>
+                <h2 className="text-[25px] md:text-[16px] " >Take your Easybank card wherever you go</h2>
+                <p className="text-[16px] md:text-[12px] " >We want you to enjoy your travels. This is why we don’t charge any fees on purchases 
+                while you’re abroad. We’ll even show you …</p>
+              </div>
             </div>
-          </div>
-          <div className="my-[7%] bg-white rounded-[10px] shadow-xl">
-            <img className="rounded-t-[10px]" src={confetti} alt=''/>
-            <div className="p-[7%]">
-              <h4 className="text-[12px]">By Claire Robinson</h4>
-              <h2 className="text-[25px] " >Our invite-only Beta accounts are now live!</h2>
-              <p className="text-[16px] " >After a lot of hard work by the whole team, we’re excited to launch our closed beta. 
-              It’s easy to request an invite through the site ...</p>
+            <div className="my-[7%] md:w-[80%] md:mx-[1%] bg-white rounded-[10px] shadow-xl">
+              <img className="rounded-t-[10px]" src={confetti} alt=''/>
+              <div className="p-[7%]">
+                <h4 className="md:text-[10px] text-[12px]">By Claire Robinson</h4>
+                <h2 className="text-[25px] md:text-[16px] " >Our invite-only Beta accounts are now live!</h2>
+                <p className="text-[16px] md:text-[12px] " >After a lot of hard work by the whole team, we’re excited to launch our closed beta. 
+                It’s easy to request an invite through the site ...</p>
+              </div>
             </div>
           </div>
       </div>
